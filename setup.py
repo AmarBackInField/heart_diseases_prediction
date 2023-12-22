437faf37d00651239157a4421397063c168f60fe
AmarBackInField <126746349+AmarBackInField@users.noreply.github.com>
2023-12-22 11:37:28 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Heart_Disease',
+    version='0.0.1',
+    author='Amar',
+    author_email='amarc8399@gmail.com',
+    packages=find_packages(),
+)
